fe7aab2e9c08034e264792dcabf634096d3443fe
unknown <preyasu.official@gmail.com>
2024-06-12 22:45:18 +0530

    camera

--- test.py
+++ test.py
@@ -1,27 +1,48 @@
 import cv2
 import numpy as np
+import time
 
-img = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
+cap = cv2.VideoCapture(0)
+# img = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
 
 params = cv2.SimpleBlobDetector_Params()
-
-params.filterByArea = False
-params.minArea = 1
-params.filterByCircularity = False
+params.filterByArea = True
+params.minArea = 20
+params.filterByCircularity = True
 params.minCircularity = 0.5
 params.filterByConvexity = False
 params.filterByInertia = True
-params.minInertiaRatio = 0.4
-
+params.minInertiaRatio = 0.7
 detector = cv2.SimpleBlobDetector_create(params)
 
-keypoints = detector.detect(img)
-# img_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+prev_time = time.time()
+frame_count = 0
+while True:
+    ret, frame = cap.read()
 
-for keypoint in keypoints:
-    center = (int(keypoint.pt[0]), int(keypoint.pt[1]))
-    cv2.circle(img, center, 5, (0, 0, 255), -1)
+    if ret:
+        curr_time = time.time()
+        if curr_time - prev_time >= 1:  # Calculate frame rate every 1 second
+            frame_rate = frame_count / (curr_time - prev_time)
+            print(f"Frame Rate: {frame_rate:.2f} fps")
+            prev_time = curr_time
+            frame_count = 0
+        frame_count += 1
 
-cv2.imshow('Blob Detection', img)
-cv2.waitKey(0)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        keypoints = detector.detect(gray)
+        img_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        # for keypoint in keypoints:
+        #     center = (int(keypoint.pt[0]), int(keypoint.pt[1]))
+        #     cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+        # cv2.imshow('Blob Detection', frame)
+        cv2.imshow('Blob Detection', img_with_keypoints)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+
 cv2.destroyAllWindows()
